4044c0a28cecb9c2c9cb117b670d599c0bf7f8e4
javakung <javakung@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-24 18:18:32 +0000

    git-svn-id: https://plugins.svn.wordpress.org/qr-encoder/trunk@266905 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/qr.js
+++ js/qr.js
@@ -1,39 +1,39 @@
 var handleEvent = {
-	start:function(eventType, args){
+	qrstart:function(eventType, args){
 	// do something when startEvent fires.
 	document.getElementById('qrgenerate').innerHTML = "<center><img src=/wp-content/plugins/qr-encoder/images/wait.gif></center>";
 	},
 
-	complete:function(eventType, args){
+	qrcomplete:function(eventType, args){
 	// do something when completeEvent fires.
 		document.qrencoder.q.select();
 	},
 
-	success:function(eventType, args){
+	qrsuccess:function(eventType, args){
 	// do something when successEvent fires.
 		if(args[0].responseText !== undefined){
 			document.getElementById('qrgenerate').innerHTML = args[0].responseText;
-			document.qrencoder.q.select();
+			document.qrencoder.qr.select();
 		}
 	},
 
-	failure:function(eventType, args){
+	qrfailure:function(eventType, args){
 	// do something when failureEvent fires.
 		alert('answering system error');
 	},
 
-	abort:function(eventType, args){
+	qrabort:function(eventType, args){
 	// do something when abortEvent fires.
 	}
 };
 
-var callback = {
+var qrcallback = {
 	customevents:{
-		onStart:handleEvent.start,
-		onComplete:handleEvent.complete,
-		onSuccess:handleEvent.success,
-		onFailure:handleEvent.failure,
-		onAbort:handleEvent.abort
+		onStart:handleEvent.qrstart,
+		onComplete:handleEvent.qrcomplete,
+		onSuccess:handleEvent.qrsuccess,
+		onFailure:handleEvent.qrfailure,
+		onAbort:handleEvent.qrabort
 	},
 	scope:handleEvent,
  	argument:["foo","bar","baz"]
@@ -41,11 +41,11 @@ var callback = {
 
 
 function makeRequest(){
-	var q = encodeURIComponent(document.getElementById("q").value);
+	var q = encodeURIComponent(document.getElementById("qr").value);
 	if(q!=""){
 		var sUrl = "/wp-content/plugins/qr-encoder/abdul.php";
 		var data = "q="+q;
-		var request = YAHOO.util.Connect.asyncRequest('POST', sUrl, callback,data);
+		var request = YAHOO.util.Connect.asyncRequest('POST', sUrl, qrcallback,data);
 	}
 }
 
